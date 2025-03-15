1550daad6027981569536a5696a6b3b31670bf2a
pratik manjarekar <pratikmanjarekar20488@gmail.com>
2025-03-15 18:42:52 +0530

    added

--- Diff.java
+++ Diff.java
@@ -1,3 +1,8 @@
-public class Diff {
-    
+public class Multiply {
+    public static void main (String[]args){
+        int a=5;
+        int b=6;
+        int multiply=a*b;
+        System.out.print(multiply);
+    }  
 }
